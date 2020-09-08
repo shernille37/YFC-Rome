ed9b0bd8ca6db32c9cdbbcf91c82fd1d2e303626
Shernille Licud <shernillelicud@Shernilles-MBP.station>
2020-09-08 02:24:36 +0200

    Updated

--- client/src/components/content/Members/MembersShowcase.js
+++ client/src/components/content/Members/MembersShowcase.js
@@ -4,7 +4,7 @@ const MembersShowcase = () => {
   return (
     <Fragment>
       <div className='membersShowcase flex justify-center items-center'>
-        <p className='text-white font-allerta text-4xl lg:text-7xl md:text-5xl '>
+        <p className='text-white font-allerta text-4xl lg:text-7xl md:text-5xl text-center'>
           Members of YFC Rome
         </p>
       </div>
